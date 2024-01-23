15c349df93913a32c312591ea50b8d973df22aa8
Luke Schaefer <lukeschaefer17@gmail.com>
2024-01-23 16:40:55 -0600

    initial simple page navigation

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -9,15 +9,31 @@ class HtmxHttpRequest(HttpRequest):
     htmx: HtmxDetails
 
 
+# # Create your views here.
+# @require_GET
+# def index(request: HtmxHttpRequest) -> HttpResponse:
+#     # this shows the trigger event in the django server terminal
+#     print(request.htmx.trigger)
+#     if request.htmx.trigger == "replace_button":
+#         context = {"test": "Button pressed!"}
+#         # this entire html file is returned and placed into the target of the call
+#         return render(request, "components/_partial.html", context)
+#     else:
+#         context = {"test": "Initial page load"}
+#         return render(request, "dashboard/index.html", context)
+    
 # Create your views here.
 @require_GET
 def index(request: HtmxHttpRequest) -> HttpResponse:
     # this shows the trigger event in the django server terminal
     print(request.htmx.trigger)
-    if request.htmx.trigger == "replace_button":
-        context = {"test": "Button pressed!"}
-        # this entire html file is returned and placed into the target of the call
-        return render(request, "components/_partial.html", context)
+    if request.htmx.trigger == "home_page_button":
+        context = {"test": "Home Page Switch", "title": "Home"}
+        return render(request, "dashboard/index.html", context)
+    elif request.htmx.trigger == "search_page_button":
+        context = {"test": "Search Page Switch", "title": "Search"}
+        return render(request, "dashboard/index.html", context)
     else:
-        context = {"test": "Initial page load"}
+        # landing page
+        context = {"test": "Initial landing", "title": "Home"}
         return render(request, "dashboard/index.html", context)
