a9fd6d7ba24a18bc850d04db721100a167c5281e
Luke Schaefer <lukeschaefer17@gmail.com>
2024-01-28 16:14:30 -0600

    back button func

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -21,25 +21,36 @@ class HtmxHttpRequest(HttpRequest):
 #     else:
 #         context = {"test": "Initial page load"}
 #         return render(request, "dashboard/index.html", context)
-    
 # Create your views here.
+    
+previous_render = "home_page.html"
+previous_context = {"test": "Initial landing", "title": "Welcome to Unredacted"}
+
 @require_GET
 def index(request: HtmxHttpRequest) -> HttpResponse:
     # this shows the trigger event in the django server terminal
     print(request.htmx.trigger)
+    global previous_context
+    global previous_render
     if request.htmx.trigger == "home_page_button":
-        context = {"test": "Home Page Switch", "title": "Home"}
-        return render(request, "home_page.html", context)
+        context = previous_context = {"test": "Home Page Switch", "title": "Home"}
+        new_render = previous_render = "home_page.html"
     elif request.htmx.trigger == "search_page_button":
-        context = {"test": "Search Page Switch", "title": "Search"}
-        return render(request, "search_page.html", context)
+        context = previous_context = {"test": "Search Page Switch", "title": "Search"}
+        new_render = previous_render = "search_page.html"
     elif request.htmx.trigger == "document_search_button":
-        context = {"test": "document search results", "title": "Document Search Results Page"}
-        return render(request, "search_results.html", context)
+        context = previous_context = {"test": "document search results", "title": "Document Search Results Page"}
+        new_render = previous_render = "search_results.html"
     elif request.htmx.trigger == "document_page_button":
         context = {"test": "document search", "title": "Document Page"}
-        return render(request, "document_page.html", context)
+        new_render = "document_page.html"
+    elif request.htmx.trigger == "back_button":
+        context = previous_context
+        new_render = previous_render
     else:
         # landing page
-        context = {"test": "Initial landing", "title": "Welcome to Unredacted"}
-        return render(request, "landing_page.html", context)
+        context = previous_context = {"test": "Initial landing", "title": "Welcome to Unredacted"}
+        new_render = "landing_page.html"
+        previous_render = "home_page.html"
+    
+    return render(request, new_render, context) 
