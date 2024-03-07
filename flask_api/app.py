a0d6c00c036e32eb26db18423cd2643154093875
Cameron Day <42944700+cday960@users.noreply.github.com>
2024-03-06 21:36:49 -0600

    Basic document search endpoint (#8)
    * added webapp search endpoint
    
    * added datetime to document
    
    * added custom encoder for datetime
    
    * changed uuid to naId

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,40 +1,87 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from dotenv import load_dotenv
 import os
 import requests
+import json
 
-app = Flask(__name__)
-CORS(app)
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+from app_factory import FlaskAppWrapper
+from models import Document
+
 
 load_dotenv()
 
 api_url = "https://catalog.archives.gov/api/v2/"
-# api_key = ""
 api_key = str(os.getenv("API_KEY"))
+headers = {"Content-Type": "application/json", "x-api-key": api_key}
+
+flask_app = Flask(__name__)
+app = FlaskAppWrapper(flask_app)
 
 
-@app.route("/")
-def home():
-    return "Welcome to the Unredacted API!"
+@app.route(
+    "/webapp/search/<string:search_parameters>/<int:result_limit>", methods=["GET"]
+)
+@app.route("/webapp/search/<string:search_parameters>", methods=["GET"])
+def webapp_search(search_parameters: str, result_limit: int = 20):
+    """
+    127.0.0.1:5000/webapp/search/<search_parameters>
+    Example: 127.0.0.1:5000/webapp/search/john+f+kennedy
+        + acts as a space in a url
+    """
+    doc_list = []
+
+    url = f"{api_url}records/search?q={search_parameters}&limit={result_limit}"
+
+    json_response = json.loads(requests.get(url, headers=headers).text)
+
+    for result in json_response["body"]["hits"]["hits"]:
+        # print(json.dumps(json_response, indent=2))
+
+        title = result["_source"]["record"]["title"]
+        naId = result["_source"]["metadata"]["controlGroup"]["naId"]
+        filename = result["_source"]["metadata"]["fileName"]
+        doc_type = result["_type"]
+        date = result["_source"]["metadata"]["ingestTime"]
+        doc = Document(title, naId, filename, doc_type, date)
+
+        # Some records have no digitalObjects
+        try:
+            doc.digitalObjects = [
+                {
+                    "filename": obj.get("objectFilename"),
+                    "url": obj.get("objectUrl"),
+                    "type": obj.get("objectType"),
+                    "description": obj.get("objectDescription"),
+                }
+                # limiting to five to make data a little easier to sift through
+                for obj in result["_source"]["record"]["digitalObjects"][0:5]
+            ]
+        except KeyError:
+            print(
+                f"ERROR: the document has no digital objects"
+                f"(naId: {naId}, filetype: {filename[-3:]})"
+            )
+            doc.digitalObjects = []
+
+        doc_list.append(doc.to_dict())
+
+    return jsonify({"data": doc_list})
 
 
-@app.route("/webapp/search", methods=["POST"])
-def webapp_search():
-    request_str = str(request.data)
-    request_json = request.get_json()
+@app.route("/webapp/records/<string:naId>", methods=["GET"])
+def webapp_records(naId: str):
+    if naId == "" or None:
+        return jsonify({"error": "invalid naId given"}), 400
 
-    parameters = request_json["search_parameters"]
-    url = api_url + "records/search?q=" + parameters[0]
+    url = f"{api_url}records/search?naId={naId}"
 
-    headers = {"Content-Type": "application/json", "x-api-key": api_key}
+    json_response = json.loads(requests.get(url, headers=headers).text)
+    print(json.dumps(json_response, indent=2))
 
-    na_api_response = requests.get(url, headers=headers)
-    test = na_api_response.text
-    print(test)
-    return jsonify({"data": na_api_response.text})
+    return jsonify({"data": "temp"})
 
 
 if __name__ == "__main__":
     print(api_key)
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ flask_api/app_factory.py
@@ -0,0 +1,45 @@
+from handler import EndpointHandler
+
+
+class FlaskAppWrapper:
+    """
+    Initializes a flask app with the given config parameters
+    app: Flask app object
+    """
+
+    # configs is a variable length dict (keyword args)
+    def __init__(self, app, **configs):
+        """
+        Initializes a Flask app with the given configuration.
+        app: Flask app object
+        configs: keyword args for Flask configuration
+        """
+        self.app = app
+        self.configs(**configs)
+        self.route = self.app.route
+
+    def configs(self, **configs):
+        for config, value in configs:
+            self.app.config[config.upper()] = value
+
+    # adds endpoint to the Flask app
+    def add_endpoint(
+        self,
+        endpoint=None,
+        endpoint_name=None,
+        handler=None,
+        methods=["GET"],
+        *args,
+        **kwargs
+    ):
+        self.app.add_url_rule(
+            endpoint,
+            endpoint_name,
+            EndpointHandler(handler),
+            methods=methods,
+            *args,
+            **kwargs
+        )
+
+    def run(self, **kwargs):
+        self.app.run(**kwargs)
