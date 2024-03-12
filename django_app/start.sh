be86d327390be3f46535dfd3786fc1f98ee618e2
Luke Schaefer <lukeschaefer17@gmail.com>
2024-03-11 22:26:12 -0500

    Dev/luke (#12)
    * added webapp search endpoint
    
    * added datetime to document
    
    * added custom encoder for datetime
    
    * changed uuid to naId
    
    * refactor, added dockerization
    
    * Add better parameter passing
    
    ---------
    
    Co-authored-by: cday960 <c.day960@gmail.com>

--- django_app/start.sh
+++ django_app/start.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+# virtualenv --clear .venv
+# source .venv/bin/activate 
+# pip install -r requirements.txt
 python3 manage.py tailwind start &
 python3 manage.py runserver 0.0.0.0:8000
