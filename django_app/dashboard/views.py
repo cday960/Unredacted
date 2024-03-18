dfe9a8998f3aa96508fb2b9042e8a8896238ee41
Luke Schaefer <lukeschaefer17@gmail.com>
2024-03-18 01:07:46 -0500

    qupdate

--- django_app/dashboard/views.py
+++ django_app/dashboard/views.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-
-=======
-import requests
->>>>>>> 82068427c0fc0d5cc49ca6181db6eae69d1f4d4f
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, FileResponse
 from django.views.decorators.http import require_GET
 from django.views.decorators.http import require_POST
-<<<<<<< HEAD
 from .models import PageInfo, PageContext
 
 from django_htmx.middleware import HtmxDetails
@@ -20,23 +14,13 @@ page_info: PageInfo = PageInfo(
     render="home_page.html",
     context=PageContext(test="Initial landing", title="Welcome to Unredacted"),
 )
-=======
-from .models import Document, PageInfo, PageContext
-
-from django_htmx.middleware import HtmxDetails
-
->>>>>>> 82068427c0fc0d5cc49ca6181db6eae69d1f4d4f
 
 page_info: PageInfo = PageInfo(render="home_page.html", context=PageContext(test="Initial landing", title="Welcome to Unredacted"))
 
 class HtmxHttpRequest(HttpRequest):
     htmx: HtmxDetails
-<<<<<<< HEAD
 
 
-=======
-    
->>>>>>> 82068427c0fc0d5cc49ca6181db6eae69d1f4d4f
 # basic page display navigation, no arguments
 @require_GET
 def get_index(request: HtmxHttpRequest) -> HttpResponse:
@@ -44,7 +28,6 @@ def get_index(request: HtmxHttpRequest) -> HttpResponse:
     print(request.htmx.trigger)
     global page_info
     if request.htmx.trigger == "home_page_button":
-<<<<<<< HEAD
         page_info.set_render_context(
             render="home_page.html",
             context=PageContext(test="Home Page Switch", title="Home"),
@@ -54,17 +37,11 @@ def get_index(request: HtmxHttpRequest) -> HttpResponse:
             render="search_page.html",
             context=PageContext(test="Search Page Switch", title="Search"),
         )
-=======
-        page_info.set_render_context(render="home_page.html", context=PageContext(test="Home Page Switch", title="Home"))
-    elif request.htmx.trigger == "search_page_button":
-        page_info.set_render_context(render="search_page.html", context=PageContext(test="Search Page Switch", title="Search"))
->>>>>>> 82068427c0fc0d5cc49ca6181db6eae69d1f4d4f
     elif request.htmx.trigger == "back_button":
         page_info.revert()
     else:
         page_info.set_render("landing_page.html")
         page_info.previous_render = "home_page.html"
-<<<<<<< HEAD
         page_info.set_context(
             PageContext(test="Initial landing", title="Welcome to Unredacted")
         )
@@ -72,19 +49,12 @@ def get_index(request: HtmxHttpRequest) -> HttpResponse:
     return render(request, *page_info.get_render_context_dict())
 
 
-=======
-        page_info.set_context(PageContext(test="Initial landing", title="Welcome to Unredacted"))
-    
-    return render(request, *page_info.get_render_context_dict()) 
-
->>>>>>> 82068427c0fc0d5cc49ca6181db6eae69d1f4d4f
 # searching for a document
 @require_POST
 def search_docs_index(request: HtmxHttpRequest) -> HttpResponse:
     print(request.htmx.trigger)
     global page_info
     if request.htmx.trigger == "document_search_button":
-<<<<<<< HEAD
         search_query = str(request.POST.get("search_query"))
         if(len(search_query) > 1):
             search_results = get_doc_list_from_na(search_query)
@@ -129,25 +99,3 @@ def download_pdf(request: HtmxHttpRequest, url: str) -> HttpResponse:
     response = HttpResponse(pdf_content, content_type="application/pdf")
     response["Content-Disposition"] = 'attachment; filename="document.pdf"'
     return response
-=======
-        search_query: str = str(request.POST.get('search_query')).replace(' ', '+')
-        # # process the request through the API
-        # url = f"127.0.0.1:5000/webapp/search/{search_query}"
-        # response = requests.get(url)
-        # search_results: list[Document] = response.json()
-        search_results = [Document(title=search_query, naId=5)]
-        page_info.set_render_context(render="search_results.html", context=PageContext(test="document search results", title="Document Search Results Page", data={"search_results": search_results, "search_query": search_query}))
-    return render(request, *page_info.get_render_context_dict()) 
-
-@require_GET
-def display_doc_index(request: HtmxHttpRequest, naId: int, title: str) -> HttpResponse:
-    print(request.htmx.trigger)
-    global page_info
-    if request.htmx.trigger == "document_page_button":
-        # url = f"127.0.0.1:5000/webapp/search/{search_query}"
-        # response = requests.get(url)
-        # document: Document = response.to_dict[1]
-        document : Document = Document(title=title, naId=naId)
-        page_info.set_render_context(render="document_page.html", context=PageContext(test=f"Individual document load: naId = {document.naId}", title=f"Viewing {document.title}", data={"document": document}))
-    return render(request, *page_info.get_render_context_dict()) 
->>>>>>> 82068427c0fc0d5cc49ca6181db6eae69d1f4d4f
