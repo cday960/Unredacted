c26ef1caaf466752ca63c0d532fe189d67b9b6ad
Luke Schaefer <lukeschaefer17@gmail.com>
2024-03-19 16:44:27 -0500

    qupdate

--- django_app/utils/nlp.py
+++ django_app/utils/nlp.py
@@ -1,5 +1,5 @@
-from decouple import config
 import io
+import os
 import PyPDF2
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -11,21 +11,20 @@ from ibm_watson.natural_language_understanding_v1 import (
     KeywordsOptions,
 )
 
+NLP_API_KEY = str(os.getenv("NLP_API_KEY"))
+NLP_API_URL = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/1645185e-ee50-492f-b1f3-5093f5094d51"
 
-# NLP_API_KEY = config("NLP_API_KEY")
-# NLP_API_URL = config("NLP_API_URL")
+print(f"National Archives API Key: {NLP_API_KEY}")
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version="2022-04-07", authenticator=IAMAuthenticator(NLP_API_KEY)
+)
+natural_language_understanding.set_service_url(NLP_API_URL)
+
 # SAMPLE_TEXT_TO_ANALYZE = ""
-
 # with open("NLP/SAMPLE_TEXT.txt", "r") as file:
 #     text = " ".join(line.rstrip() for line in file)
 #     SAMPLE_TEXT_TO_ANALYZE = text
-
-# authenticator = IAMAuthenticator(NLP_API_KEY)
-# natural_language_understanding = NaturalLanguageUnderstandingV1(
-#     version="2022-04-07", authenticator=authenticator
-# )
-
-# natural_language_understanding.set_service_url(NLP_API_URL)
 
 # response = natural_language_understanding.analyze(
 #     text=SAMPLE_TEXT_TO_ANALYZE,
@@ -55,7 +54,15 @@ def extract_pdf_text(pdf_bytes: bytes):
     return text
 
 def summarize_pdf(text: str):
-    '''
-    ADD SUMMARY HERE!!!
-    '''
+    if len(text) > 0:
+        response = natural_language_understanding.analyze(
+            text=text,
+            features=Features(
+                entities=EntitiesOptions(sentiment=True, limit=30),
+                categories=CategoriesOptions(limit=10),
+                concepts=ConceptsOptions(limit=10),
+                keywords=KeywordsOptions(limit=20),
+            ),
+        ).get_result()
+        print(response)
     return None
